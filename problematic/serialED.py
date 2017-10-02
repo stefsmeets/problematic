f4b6a3f3646455a2e9328f7555069370db3f4d7c
Stef Smeets <stef.smeets@mmk.su.se>
2017-10-02 17:58:20 +0200

    Add docstrings

--- problematic/serialED.py
+++ problematic/serialED.py
@@ -62,6 +62,14 @@ class serialED(pc.ElectronDiffraction):
         self.load_extras()
 
     def get_direct_beam_position(self, sigma=10):
+        """Get the position of the direct beam in all frames using the blurring method
+
+        Sigma: int
+            Standard deviation for the gaussian kernel
+
+        Returns:
+            BaseSignal1D"""
+
         method = "blur"
         d = {"sigma": sigma,
         "method": method,
@@ -74,6 +82,16 @@ class serialED(pc.ElectronDiffraction):
         return centers
 
     def apply_stretch_correction(self, centers, azimuth=-6.61, amplitude=2.43, inplace=False):
+        """Applies a stretch correction to all images
+
+        azimuth: float
+            Direction of the azimuth in degrees with respect to the vertical axis (TODO: check this)
+        amplitude: float
+            The difference in percent between the long and short axes
+
+        Returns:
+            serialED(BaseSignal2D)"""
+
         d = {"azimuth": azimuth,
         "amplitude": amplitude,
         "date": str(datetime.datetime.now()),
@@ -83,6 +101,14 @@ class serialED(pc.ElectronDiffraction):
         return self.map(apply_stretch_correction, azimuth=azimuth, amplitude=amplitude, center=centers, inplace=inplace)
 
     def remove_background(self, footprint=19, inplace=False):
+        """Removes the background of each frame using a median filter
+
+        footprint:
+            size of the window for determining the median
+
+        Returns:
+            serialED(BaseSignal2D)"""
+
         method = "median"
         d = {"footprint": footprint,
         "method": method,
@@ -93,6 +119,24 @@ class serialED(pc.ElectronDiffraction):
         return super().remove_background(method=method, footprint=footprint, inplace=inplace)
 
     def find_peaks_and_clean_images(self, min_sigma=4, max_sigma=5, threshold=1, min_size=50, inplace=False):
+        """Find regions of connected pixels using regionprops method, and use those to remove noise
+        (i.e. clean the images). This is done by setting the intensities in areas areas that do not 
+        belong to the connected regions are set to zero.
+
+        min_sigma : int, float
+            Standard deviation for the minimum gaussian convolution
+        max_sigma : int, float
+            Standard deviation for the maximum gaussian convolution
+        threshold : int, float
+            Minimum difference in intensity
+        min_size : int
+            Minimum size in pixels of blob
+
+        Returns:
+            serialED(BaseSignal2D)
+
+        regionprops are stored to self._props_collection
+        """
         d = {
         "min_sigma": min_sigma,
         "max_sigma": max_sigma,
@@ -116,6 +160,39 @@ class serialED(pc.ElectronDiffraction):
         return self.map(im_reconstruct_func, props=props_collection, inplace=inplace)
 
     def find_orientations(self, indexer, centers, nsolutions=25, filter1d=False, nprojs=100):
+        """Find the orientations for all the images using a brute force forward projection method
+
+        indexer: indexer.Indexer
+             See `indexer.Indexer` documention for how to set this up
+        centers: BaseSignal1D
+            List of centers of the diffraction patterns
+        nsolutions: int
+            Number of best solutions to keep for each fram
+        filter1d: bool
+            Use a powder pattern as a first selection criterion to determine
+            if a frame is good or nto
+        nprojs: int
+            Number of projections to keep, corresponding to filter1d
+
+        Returns:
+            BaseSignal1D containing a list of `nsolutions` namedtuples
+
+        Example:
+            pixelsize = 0.00433
+            dmin, dmax = 1.0, 10.0
+            params = (24.3450,)
+            spgr = "Fd-3m"
+            name = "FAU"
+            projector = Projector.from_parameters(params, spgr=spgr, name=name, dmin=dmin, dmax=dmax, thickness=thickness)
+            indexer = Indexer.from_projector(projector, pixelsize=pixelsize)
+            orientations = ed.find_orientations(indexer, centers)
+
+            # visualize results
+            ed.orientation_explorer(indexer, orientations)
+
+            # save results
+            serialED.io.save_orientations(orientations)
+        """
         d = indexer.to_dict()
         d["date"] = str(datetime.datetime.now())
         d["func"] = "indexer.find_orientation"
@@ -128,6 +205,38 @@ class serialED(pc.ElectronDiffraction):
 
     def refine_orientations(self, indexer, orientation_collection, sort=True, method="powell", 
                             vary_scale=True, vary_center=True, vary_alphabeta=True):
+        """Refine the orientations obtained using serialED.find_orientations
+
+        indexer: indexer.Indexer
+             See `indexer.Indexer` documention for how to set this up
+        orientation_collection: BaseSignal1D
+            Collection of orientations
+        sort: bool
+            Sort the orientations
+        method: str
+            Method to use for the least-squares minimization
+        vary_scale: bool
+            Refine the scale (1/pixelsize)
+        vary_center: bool
+            Refine the center of the beam
+        vary_alphabeta: bool
+            Refine the orientation of the crystals
+
+        Returns:
+            BaseSignal1D containing a list of namedtuples
+
+        Example:
+            orientations = ed.find_orientations(indexer, centers)
+
+            # refine the orientations
+            refined_orientations = ed.refine_orientations(indexer, orientations)
+
+            # visualize results
+            ed.orientation_explorer(indexer, refined_orientations)
+
+            # save results
+            serialED.io.save_orientations(refined_orientations)
+        """
         d = indexer.to_dict()
         d["vary_scale"] = vary_scale
         d["vary_center"] = vary_center
@@ -144,6 +253,20 @@ class serialED(pc.ElectronDiffraction):
         return orientation_collection
 
     def orientation_explorer(self, indexer, orientations, imshow_kwargs={}):
+        """Visualize a collection of orientations
+
+        indexer: indexer.Indexer
+             See `indexer.Indexer` documention for how to set this up
+        orientation_collection: BaseSignal1D
+            Collection of orientations
+        imshow_kwargs: dict
+            List of keyword arguments to pass to plt.imshow      
+
+        Example
+            orientations = ed.find_orientations(indexer, centers)
+
+            # visualize results
+            ed.orientation_explorer(indexer, orientations, imshow_kwargs={"vmax":100, "cmap":"gray"})"""
         from peakexplorer import PeakExplorer
         default_imshow_kwargs = {"cmap":"gray"}
         default_imshow_kwargs.update(imshow_kwargs)
@@ -151,6 +274,19 @@ class serialED(pc.ElectronDiffraction):
         peakexp.interactive(self)
 
     def plot_orientations(self, indexer, orientation_collection, n=25, vmin=None, vmax=None, ax=None):
+        """plot orientations using matplotlib
+
+        indexer: indexer.Indexer
+             See `indexer.Indexer` documention for how to set this up
+        orientation_collection: BaseSignal1D
+            Collection of orientations
+        n: int
+            Number of best orientations to show
+        vmax, vmin: float
+            Passed to imshow
+        ax: matplotlib.Axes
+        """
+
         import matplotlib.pyplot as plt
         scores = np.array([ori[0].score for ori in orientation_collection.data])
         ix = np.argsort(scores)[::-1]
@@ -163,6 +299,17 @@ class serialED(pc.ElectronDiffraction):
             plt.show()
 
     def extract_intensities(self, indexer, orientations, outdir="intensities"):
+        """Extracts the intensities at the predicted spots
+
+        indexer: indexer.Indexer
+             See `indexer.Indexer` documention for how to set this up
+        orientation_collection: BaseSignal1D
+            Collection of orientations
+        outdir: str
+            Directory where to store the hkl files with the extracted intensities
+
+        Returns:
+            BaseSignal1D: Contains lists of extracted intensities"""
         d = {}
         d["outdir"] = outdir
         d["date"] = str(datetime.datetime.now())
@@ -184,6 +331,7 @@ class serialED(pc.ElectronDiffraction):
         return intensities_collection
 
     def export_indexing_results(s, fname="orientations.ycsv"):
+        """Export the indexing results to a ycsv file"""
         import pandas as pd
         import yaml
         d = s.metadata.Processing["find_orientations"].as_dictionary()
@@ -218,6 +366,7 @@ class serialED(pc.ElectronDiffraction):
             return False
 
     def save(self, filename, *args, **kwargs):
+        """Save current progress"""
         root, ext = os.path.splitext(filename)
         d = {}
         if self._centers:
